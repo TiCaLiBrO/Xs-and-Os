40d2c7257079515d75bbf5d768018d84af4120ac
Tyson <tysonhayter3154@gmail.com>
2026-05-28 21:16:04 -0400

    Added the new version, v0.7

--- X and O.py
+++ X and O.py
@@ -12,19 +12,24 @@ CLEAR  = "\033[H\033[2J"
 X = RED  + "X" + WHITE
 O = BLUE + "O" + WHITE
 
+WIN_TIMER      = 0.5
+MOVE_TIMER     = 0.2
+SPECTATE_TIMER = 0.1
+## Add a spectating win timer
+## Rename Spectate timer to spectate_move_timer.
+
 VALID_INPUTS = ["top left", "top", "top right", "left", "middle", "right", "bottom left", "bottom", "bottom right"]
 remaining_inputs = ["top left", "top", "top right", "left", "middle", "right", "bottom left", "bottom", "bottom right"]
 
+print(YELLOW + "Modes involding the learning AI and optimal AI are not implemented yet." + WHITE)
 
-print(YELLOW + "The following are the valid inputs for placing your " + X + YELLOW + " or " + O + YELLOW + " on the board:")
-print(YELLOW + "top left, top, top right, left, middle, right, bottom left, bottom, bottom right" + WHITE)
 
 
 last_player1 = O
-
-turn    = X
-round   = 1
-playing = True
+turn         = X
+round        = 1
+playing      = True
+game_counter = 1
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
@@ -36,6 +41,90 @@ O_talley = 0
 "┃┃┃"
 "┃┃┃┃"
 "╋╋╋╋"
+
+
+game_mode = None
+
+def start_menu():
+    juno = None
+    while juno != "1" or juno != "2" or juno != "3":
+        print("Enter a number to select a mode:")
+        print("1 -> Player Versus AI")
+        print("2 -> Player Versus Player")
+        print("3 -> Spectate AI Versus AI")
+        juno = input()
+        if juno == "1":
+            print("Enter a number to select a mode:")
+            print("1 -> Player versus Weak AI")
+            print("2 -> Player versus Learning AI")
+            print("3 -> Player versus Optimal AI")
+            while juno != "1" or juno != "2" or juno != "3":
+                juno = input()
+                if juno == "1":
+                    return "Player versus Weak AI"
+                elif juno == "2":
+                    return "Player versus Learning AI"
+                elif juno == "3":
+                    return "Player versus Optimal AI"
+                else:
+                    print("Invalid input. Try again.")
+                    juno = None
+        elif juno == "2":
+            return "Player versus Player"
+        elif juno == "3":
+            print("Enter a number to select a mode:")
+            print("1 -> Spectate Weak AI versus Weak AI")
+            print("2 -> Spectate Weak AI versus Learning AI")
+            print("3 -> Spectate Weak AI versus Optimal AI")
+            print("4 -> Spectate Learning AI versus Learning AI")
+            print("5 -> Spectate Learning AI versus Optimal AI")
+            print("6 -> Spectate Optimal AI versus Optimal AI")
+            while juno != "1" or juno != "2" or juno != "3" or juno != "4" or juno != "5" or juno != "6":
+                juno = input()
+                if juno == "1":
+                    return "Weak AI verusus Weak AI"
+                elif juno == "2":
+                    return "Weak AI versus Learning AI"
+                elif juno == "3":
+                    return "Weak AI versus Optimal AI"
+                elif juno == "4":
+                    return "Learning AI versus Learning AI"
+                elif juno == "5":
+                    return "Learning AI versus Optimal AI"
+                elif juno == "6":
+                    return "Optimal AI versus Optimal AI"
+                else:
+                    print("Invalid input. Try again.")
+                    juno = None
+        else:
+            print("Invalid input. Try again.")
+            juno = None
+
+game_mode = start_menu()
+print(CLEAR, end="")
+
+
+# we now have 10 game modes. 
+#  1 Working
+#  2 --- Need Learning AI
+#  3 --- Need Optimal AI
+#  4 Workable
+#  5 Workable
+#  6 --- Need Learning AI
+#  7 --- Need Optimal AI
+#  8 --- Need Learning AI
+#  9 --- Need Learning AI & Optimal AI
+# 10 --- Need Optimal AI
+
+
+
+
+
+
+print(YELLOW + "The following are the valid inputs for placing your " + X + YELLOW + " or " + O + YELLOW + " on the board:")
+print(YELLOW + "top left, top, top right, left, middle, right, bottom left, bottom, bottom right" + WHITE)
+
+
 
 def print_X_talley():
     global X_talley
@@ -221,8 +310,11 @@ def check_draw():
 
 
 
+
+
 def print_board():
-    print("round " + str(round))
+    print("game:  " + str(game_counter))
+    print("round: " + str(round))
     print(board[0] + " ┃ " + board[1] + " ┃ " + board[2])
     print("━━╋━━━╋━━", end=""); print_X_talley()
     print(board[3] + " ┃ " + board[4] + " ┃ " + board[5])
@@ -231,11 +323,8 @@ def print_board():
 
 
 
-def get_input():
-    global turn
-    global round
-    global remaining_inputs
-    VALID_INPUTS = ["top left", "top", "top right", "left", "middle", "right", "bottom left", "bottom", "bottom right"]
+def get_player_versus_weak_ai_input():
+    global last_player1, turn, round, remaining_inputs, MOVE_TIMER, VALID_INPUTS
     user_input = None
     if turn == X:
         while user_input not in remaining_inputs:
@@ -243,15 +332,161 @@ def get_input():
             if user_input not in remaining_inputs:
                 print("Invalid input. Please try again.")
         turn = O
+        if last_player1 == O:
+            round += 1
         remaining_inputs.remove(user_input)
         return user_input
     if turn == O:
         print(O + " to move: ", end="")
+        sleep(MOVE_TIMER)
         turn   = X
-        round += 1
+        if last_player1 == X:
+            round += 1
         choice = random.choice(remaining_inputs)
         remaining_inputs.remove(choice)
         return choice
+
+def player_versus_weak_ai_game_loop():
+    global X_talley, O_talley, game_counter, WIN_TIMER, playing, turn
+    while True:
+        while playing:
+            print_board()
+            board[get_mark_position(get_player_versus_weak_ai_input())] = turn
+            if check_win() == X:
+                playing = False
+            if check_win() == O:
+                playing = False
+            if check_draw():
+                playing = False
+            print(CLEAR, end="")
+        if check_win() == X:
+            print(X + " wins!")
+            game_counter += 1
+            X_talley     += 1
+            sleep(WIN_TIMER)
+        if check_win() == O:
+            print(O + " wins!")
+            game_counter += 1
+            O_talley     += 1
+            sleep(WIN_TIMER)
+        if check_draw():
+            print("It's a draw!")
+            game_counter += 1
+            sleep(WIN_TIMER)
+        reset_everything()
+
+
+
+def get_player_versus_player_input():
+    global last_player1, turn, round, remaining_inputs, MOVE_TIMER, VALID_INPUTS
+    user_input = None
+    if turn == X:
+        while user_input not in remaining_inputs:
+            user_input = input(X + " to move: ")
+            if user_input not in remaining_inputs:
+                print("Invalid input. Please try again.")
+        turn = O
+        if last_player1 == O:
+            round += 1
+        remaining_inputs.remove(user_input)
+        return user_input
+    if turn == O:
+        while user_input not in remaining_inputs:
+            user_input = input(O + " to move: ")
+            if user_input not in remaining_inputs:
+                print("Invalid input. Please try again.")
+        turn = X
+        if last_player1 == X:
+            round += 1
+        remaining_inputs.remove(user_input)
+        return user_input
+
+def player_versus_player_game_loop():
+    global X_talley, O_talley, game_counter, WIN_TIMER, playing, turn
+    while True:
+        while playing:
+            print_board()
+            board[get_mark_position(get_player_versus_player_input())] = turn
+            if check_win() == X:
+                playing = False
+            if check_win() == O:
+                playing = False
+            if check_draw():
+                playing = False
+            print(CLEAR, end="")
+        if check_win() == X:
+            print(X + " wins!")
+            game_counter += 1
+            X_talley     += 1
+            sleep(WIN_TIMER)
+        if check_win() == O:
+            print(O + " wins!")
+            game_counter += 1
+            O_talley     += 1
+            sleep(WIN_TIMER)
+        if check_draw():
+            print("It's a draw!")
+            game_counter += 1
+            sleep(WIN_TIMER)
+        reset_everything()
+
+
+
+
+def get_weak_ai_versus_weak_ai_input():
+    global last_player1, turn, round, remaining_inputs, SPECTATE_TIMER, VALID_INPUTS
+    user_input = None
+    if turn == X:
+        print(X + " to move: ", end="")
+        sleep(SPECTATE_TIMER)
+        turn   = O
+        if last_player1 == O:
+            round += 1
+        choice = random.choice(remaining_inputs)
+        remaining_inputs.remove(choice)
+        return choice
+    if turn == O:
+        print(O + " to move: ", end="")
+        sleep(SPECTATE_TIMER)
+        turn   = X
+        if last_player1 == X:
+            round += 1
+        choice = random.choice(remaining_inputs)
+        remaining_inputs.remove(choice)
+        return choice
+
+def weak_ai_versus_weak_ai_game_loop():
+    global X_talley, O_talley, game_counter, WIN_TIMER, playing, turn
+    while True:
+        while playing:
+            print_board()
+            board[get_mark_position(get_weak_ai_versus_weak_ai_input())] = turn
+            if check_win() == X:
+                playing = False
+            if check_win() == O:
+                playing = False
+            if check_draw():
+                playing = False
+            print(CLEAR, end="")
+        if check_win() == X:
+            print(X + " wins!")
+            game_counter += 1
+            X_talley     += 1
+            sleep(WIN_TIMER)
+        if check_win() == O:
+            print(O + " wins!")
+            game_counter += 1
+            O_talley     += 1
+            sleep(WIN_TIMER)
+        if check_draw():
+            print("It's a draw!")
+            game_counter += 1
+            sleep(WIN_TIMER)
+        reset_everything()
+
+
+
+
 
 
 def get_mark_position(position):
@@ -263,13 +498,51 @@ def get_mark_position(position):
 
 
 
+
+
+
+
+
+
+
+
+get_input = None
+
+
+def choose_game_mode():
+    global game_mode, get_input
+    if game_mode == "Player versus Weak AI":
+        player_versus_weak_ai_game_loop()
+    if game_mode == "Player versus Learning AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Player versus Optimal AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Player versus Player":
+        player_versus_player_game_loop()
+    if game_mode == "Weak AI verusus Weak AI":
+        weak_ai_versus_weak_ai_game_loop()
+    if game_mode == "Weak AI versus Learning AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Weak AI versus Optimal AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Learning AI versus Learning AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Learning AI versus Optimal AI":
+        print("Not Implemented Yet.")
+    if game_mode == "Optimal AI versus Optimal AI":
+        print("Not Implemented Yet.")
+
+choose_game_mode()
+
+
+#
+"""
 def game_loop():
-    global X_talley, O_talley
+    global X_talley, O_talley, game_counter, WIN_TIMER, playing, get_input
     while True:
-        global playing
         while playing:
             print_board()
-            board[get_mark_position(get_input())] = turn
+            board[get_mark_position(get_input)] = turn
             if check_win() == X:
                 playing = False
             if check_win() == O:
@@ -279,22 +552,30 @@ def game_loop():
             print(CLEAR, end="")
         if check_win() == X:
             print(X + " wins!")
-            X_talley += 1
-            sleep(1)
+            game_counter += 1
+            X_talley     += 1
+            sleep(WIN_TIMER)
         if check_win() == O:
             print(O + " wins!")
-            O_talley += 1
-            sleep(1)
+            game_counter += 1
+            O_talley     += 1
+            sleep(WIN_TIMER)
         if check_draw():
             print("It's a draw!")
-            sleep(1)
+            game_counter += 1
+            sleep(WIN_TIMER)
         reset_everything()
-
-game_loop()
-
+"""
 
 
 
+# TO DO:
+#   - Make a learning AI
+#   - Make an optimal AI
+#   - Add spectating mode
+#   - Add an option to play against learning AI
+#   - Add an option to play against Optimal AI
 
 
  
+# New issue found. A rare bug where you cannot place a piece where you want (maybe I'm just insane though. It's super rare)
